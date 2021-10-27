dced42d474d34d8e7dec1c68bb617a5921936f1b
aungpaing98 <aungpaingcha@gmail.com>
2021-10-27 18:16:41 +0630

    finish nav bar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,16 +1,26 @@
-
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-    return (
-        <div className="navbar">
-            <div className="navtab">
-                <ul className="navlinks">
-                    <li className="nav"><Link className="navlink home" to="/">Aung Paing</Link></li>
-                    <li className="nav"><Link className="navlink work" to="/works">Works</Link></li>
-                </ul>
-                <div className="darktoggle">Dark</div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="navbar">
+      <div className="navtab">
+        <ul className="navlinks">
+          <li className="nav">
+            <Link className="navlink home" to="/">
+              <img className="navimg" src="/images/cow.png" alt="Cow" />
+              Aung Paing
+            </Link>
+          </li>
+          <li className="nav">
+            <Link className="navlink work" to="/works">
+              Works
+            </Link>
+          </li>
+        </ul>
+        <button className="darktoggle">
+            <img src="/images/moon.png" alt="Toggle Dark mode Button" />
+        </button>
+      </div>
+    </div>
+  );
 }
